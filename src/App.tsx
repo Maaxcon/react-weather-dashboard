08bd09942cfa0d467f36d5e47c7d558c4305e8f9
Maks <27834559+Maaxcon@users.noreply.github.com>
2026-04-03 17:34:41 +0300

    feat:render city component and add starting style

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
+import { CityCard } from "./components/CityCard"
+import { cities } from "./data/cities"
+import type { City } from "./type"
 function App() {
 
   return (
-    <>
-      
-    </>
+    <div className="weather">
+      <div className="weather__list">
+        {cities.map((city: City) => (
+          <div key={city.id} className="weather__block">
+            <CityCard
+              city={city}
+              onClick={() => {}}
+            />
+          </div>
+        ))}
+      </div>
+    </div>
   )
 }
 
